520449f831b58aaddbecbaff23b0671f0a652e40
unknown <paulofegueredo@gmail.com>
2021-03-16 17:41:11 -0300

    Desafios DIO

---
+++ Desafios/Java/Solução de problemas básicos/03. Números Impares.java
@@ -0,0 +1,31 @@
+/*
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+8			1
+			3
+			5
+			7
+*/
+
+import java.util.Scanner;
+
+public class MinhaTerceiraClasse {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+        for (int i =1 ; i <= num; i = i +2 ) 
+        { 
+            System.out.println(i);
+        }
+    }
+
+}
